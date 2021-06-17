0d26e3539694dc147b4f9bc50a7f0c7c41726337
Om prakash <omprakash72810@gmail.com>
2021-06-17 20:37:35 +0530

    intial commit

---
+++ portfolio/lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'MyHomePage.dart';
+import 'AboutMe.dart';
+import 'Tech.dart';
+import 'Contact.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Portfolio",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.blue[700],
+        backgroundColor: Colors.white,
+      ),
+      home: MyHomePage(),
+      routes: <String, WidgetBuilder>{
+        "/AboutMe": (context) => AboutMe(),
+        "/Tech": (context) => Tech(),
+        "/Contact": (context) => Contact(),
+      },
+    );
+  }
+}
